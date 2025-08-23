350ca8122e5668d67ca6dee5f0c764d6542682f2
Kenji Osari <kosari@mercari.com>
2025-08-23 15:44:16 +0900

    refactor: extract dependency check logic as pkg/checker

---
+++ pkg/checker/checker.go
@@ -0,0 +1,81 @@
+package checker
+
+import (
+	"bytes"
+	"errors"
+	"text/template"
+
+	"github.com/cloverrose/pkgdep/pkg/cachedregexp"
+	"github.com/cloverrose/pkgdep/pkg/inspector"
+	"github.com/cloverrose/pkgdep/pkg/orderedmap"
+)
+
+type Checker struct {
+	dependencies orderedmap.OrderedMap
+	inspector    *inspector.Inspector
+	regexpCache  *cachedregexp.CachedRegexp
+}
+
+func New(dependencies orderedmap.OrderedMap, inspector *inspector.Inspector) *Checker {
+	return &Checker{
+		dependencies: dependencies,
+		inspector:    inspector,
+		regexpCache:  cachedregexp.New(true),
+	}
+}
+
+func (c *Checker) IsAllowedDependency(from, to string) bool {
+	for fromPattern, toTemplateStrings := range c.dependencies.Iter() {
+		data, err := c.matchAndExtract(fromPattern, from)
+		if err != nil {
+			continue
+		}
+		for _, toTemplateString := range toTemplateStrings {
+			toPattern, err := buildPattern(toTemplateString, data)
+			if err != nil {
+				continue
+			}
+			re, err := c.regexpCache.Compile(toPattern)
+			if err != nil {
+				continue
+			}
+			if re.MatchString(to) {
+				c.inspector.RecordUsage(fromPattern, toTemplateString)
+				return true
+			}
+		}
+	}
+	return false
+}
+
+func (c *Checker) matchAndExtract(pattern, text string) (map[string]string, error) {
+	re, err := c.regexpCache.Compile(pattern)
+	if err != nil {
+		return nil, err
+	}
+	match := re.FindStringSubmatch(text)
+	if match == nil {
+		return nil, errors.New("no match")
+	}
+	names := re.SubexpNames()
+	data := make(map[string]string)
+	for i, name := range names {
+		if i > 0 && i < len(match) {
+			data[name] = match[i]
+		}
+	}
+	return data, nil
+}
+
+func buildPattern(templateString string, data map[string]string) (string, error) {
+	tmpl, err := template.New("example").Parse(templateString)
+	if err != nil {
+		return "", err
+	}
+	var result bytes.Buffer
+	if err := tmpl.Execute(&result, data); err != nil {
+		return "", err
+	}
+
+	return result.String(), nil
+}
